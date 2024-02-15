da5c0f76b8b8fee6d87dacaa756ca135cd4d65ed
Vaishnavi Shankar Devadig <vdevadig@hawk.iit.edu>
2024-02-15 16:09:40 -0600

    Hierarchy Added

--- template.py
+++ template.py
@@ -7,20 +7,21 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
 project_name = "cnnClassifier"
 list_of_files = [
     ".github/workflow/.gitkeep",
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/config/__init__.py",
-    f"src{project_name}/config/configuration.py",
-    f"src{project_name}/pipeline/__init__.py",
-    f"src{project_name}/entity/__init__.py",
-    f"src{project_name}/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
